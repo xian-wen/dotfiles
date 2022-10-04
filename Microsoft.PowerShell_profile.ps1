790db9f9407b52c5a2978caa18d08603e3187ec7
xian-wen <xianwen_yin@163.com>
2022-10-04 23:06:11 +0800

    Add powershell setup

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,17 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt ys
+
+# Ref: https://github.com/PowerShell/PSReadLine/blob/master/PSReadLine/SamplePSReadLineProfile.ps1
+Import-Module PSReadLine
+# Shows navigable menu of all options when hitting Tab.
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+# Searching for commands with up/down arrow is really handy. The
+# option "moves to end" is useful if you want the cursor at the end
+# of the line while cycling through history like it does w/o searching,
+# without that option, the cursor will remain at the position it was
+# when you used up arrow, which can be useful if you forget the exact
+# string you started the search on.
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
