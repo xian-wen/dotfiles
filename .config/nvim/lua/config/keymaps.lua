89c3e3c4b8120ba2c9150a3677a19438594384fb
xian-wen <xianwen_yin@163.com>
2024-12-18 01:40:48 +0800

    Set keymaps

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,95 @@
+local map = vim.keymap.set
+
+-- Buffers
+map("n", "<C-n>", "<Cmd>bnext<CR>", { desc = "Next Buffer" })
+map("n", "<C-p>", "<Cmd>bprevious<CR>", { desc = "Prev Buffer" })
+map("n", "<Leader>bb", "<Cmd>e #<CR>", { desc = "Switch to Other Buffer" })
+map("n", "<Leader>bn", "<Cmd>enew<CR>", { desc = "New Buffer" })
+map("n", "<Leader>bc", "<Cmd>bd<CR>", { desc = "Close Buffer" })
+
+-- Windows
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window" })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window" })
+-- <C-w>n: New Window
+-- <C-w>c: Close Window
+-- <C-w>o: Close Other Windows
+
+map("n", "<C-Up>", "2<C-w>+", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "2<C-w>-", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "2<C-w><lt>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "2<C-w>>", { desc = "Increase Window Width" })
+
+-- Tabs
+-- gt: Next Tab
+-- gT: Prev Tab
+map("n", "<Leader><Tab>n", "<Cmd>tabnew<CR>", { desc = "New Tab" })
+map("n", "<Leader><Tab>c", "<Cmd>tabclose<CR>", { desc = "Close Tab" })
+map("n", "<Leader><Tab>o", "<Cmd>tabonly<CR>", { desc = "Close Other Tabs" })
+
+-- Clear search
+map("n", "<Esc>", "<Cmd>nohlsearch<CR><Esc>", { desc = "Escape and Clear hlsearch" })
+
+-- Clear search, diff update and redraw
+-- Taken from runtime/lua/_editor.lua
+map(
+  "n",
+  "<Leader>ur",
+  "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
+  { desc = "Redraw / Clear hlsearch / Diff Update" }
+)
+
+-- Add undo break-points
+map("i", ",", ",<C-g>u")
+map("i", ".", ".<C-g>u")
+map("i", ";", ";<C-g>u")
+
+-- Better indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- Commenting
+map("n", "gco", "o<Esc>Vcx<Esc><Cmd>normal gcc<CR>fxa<BS>", { desc = "Add Comment Below" })
+map("n", "gcO", "O<Esc>Vcx<Esc><Cmd>normal gcc<CR>fxa<BS>", { desc = "Add Comment Above" })
+map("n", "gcA", "<Cmd>normal gco<CR><Esc>kJA", { desc = "Add Comment After" })
+
+-- Quickfix/Location List
+map("n", "]q", "<Cmd>cnext<CR>", { desc = "Next Quickfix" })
+map("n", "[q", "<Cmd>cprevious<CR>", { desc = "Prev Quickfix" })
+map("n", "]l", "<Cmd>lnext<CR>", { desc = "Next Location" })
+map("n", "[l", "<Cmd>lprevious<CR>", { desc = "Prev Location" })
+map("n", "<Leader>xq", "<Cmd>copen<CR>", { desc = "Quickfix List" })
+map("n", "<Leader>xl", "<Cmd>lopen<CR>", { desc = "Location List" })
+
+-- Diagnostic
+local diagnostic_goto = function(next, severity)
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = severity })
+  end
+end
+map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
+map("n", "<Leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+
+-- Highlights under cursor
+map("n", "<Leader>ui", vim.show_pos, { desc = "Inspect Pos" })
+map("n", "<Leader>uI", "<cmd>InspectTree<cr>", { desc = "Inspect Tree" })
+
+-- Toggle options
+map("n", "<Leader>ub",function()
+  vim.o.background = vim.o.background == "back" and "light" or "dark"
+end, { desc = "Toggle Background" })
+map("n", "<Leader>ul", "<Cmd>set number!<CR>", { desc = "Toggle Line Number" })
+map("n", "<Leader>uL", "<Cmd>set relativenumber!<CR>", { desc = "Toggle Relative Number" })
+map("n", "<Leader>us", "<Cmd>set spell!<CR>", { desc = "Toggle Spell" })
+map("n", "<Leader>uw", "<Cmd>set wrap!<CR>", { desc = "Toggle Wrap" })
+
+-- Lazy
+map("n", "<Leader>L", "<Cmd>Lazy<CR>", { desc = "Lazy" })
