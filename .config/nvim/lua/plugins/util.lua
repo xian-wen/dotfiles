9083fe8aa44089cb3966069221aaa783010830e6
xian-wen <xianwen_yin@163.com>
2024-12-19 09:35:04 +0800

    Add persistence.nvim

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -1,3 +1,39 @@
 return {
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    keys = {
+      {
+        "<leader>qs",
+        function()
+          require("persistence").load()
+        end,
+        desc = "Restore Session"
+      },
+      {
+        "<leader>qS",
+        function()
+          require("persistence").select()
+        end,
+        desc = "Select Session"
+      },
+      {
+        "<leader>ql",
+        function()
+          require("persistence").load({ last = true })
+        end,
+        desc = "Restore Last Session"
+      },
+      {
+        "<leader>qd",
+        function()
+          require("persistence").stop()
+        end,
+        desc = "Don't Save Current Session"
+      },
+    },
+    opts = {},
+  },
+
   "nvim-lua/plenary.nvim",
 }
