c0417b13346b90dafc2308b27161cf188da35e08
xian-wen <xianwen_yin@163.com>
2024-12-19 16:15:37 +0800

    Add a comma in the last line of a table

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -8,28 +8,28 @@ return {
         function()
           require("persistence").load()
         end,
-        desc = "Restore Session"
+        desc = "Restore Session",
       },
       {
         "<leader>qS",
         function()
           require("persistence").select()
         end,
-        desc = "Select Session"
+        desc = "Select Session",
       },
       {
         "<leader>ql",
         function()
           require("persistence").load({ last = true })
         end,
-        desc = "Restore Last Session"
+        desc = "Restore Last Session",
       },
       {
         "<leader>qd",
         function()
           require("persistence").stop()
         end,
-        desc = "Don't Save Current Session"
+        desc = "Don't Save Current Session",
       },
     },
     opts = {},
