3170e757b05a658b12bce4c05994a6e0f8588d52
xian-wen <xianwen_yin@163.com>
2024-12-20 21:21:54 +0800

    Change mappings of suffix_last to p

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -35,13 +35,16 @@ return {
     event = "VeryLazy",
     opts = {
       mappings = {
-        add = "gsa", -- Add surrounding in Normal and Visual modes
-        delete = "gsd", -- Delete surrounding
-        find = "gsf", -- Find surrounding (to the right)
-        find_left = "gsF", -- Find surrounding (to the left)
-        highlight = "gsh", -- Highlight surrounding
-        replace = "gsr", -- Replace surrounding
-        update_n_lines = "gsn", -- Update `n_lines`
+        add = "gsa",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+
+        suffix_next = 'n',
+        suffix_last = 'p',
       },
     },
   },
