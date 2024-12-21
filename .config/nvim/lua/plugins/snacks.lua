b7b9d177812fe389a2684e4b3be17dc024848273
xian-wen <xianwen_yin@163.com>
2024-12-21 22:50:26 +0800

    Add Snacks.lazygit

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -4,8 +4,45 @@ return {
   priority = 1000,
   keys = {
     -- Aliases
+    { "<Leader>G", "<Leader>gg", desc = "Lazygit (Root Dir)", remap = true },
     { "<C-_>", "<Leader>ft", mode = { "n", "t" }, desc = "which_key_ignore", remap = true },
     { "<C-/>", "<C-_>", mode = { "n", "t" }, desc = "Terminal (Root Dir)", remap = true },
+    -- Lazygit
+    {
+      "<Leader>gg",
+      function()
+        Snacks.lazygit({ cwd = Snacks.git.get_root() })
+      end,
+      desc = "Lazygit (Root Dir)",
+    },
+    {
+      "<Leader>gG",
+      function()
+        Snacks.lazygit()
+      end,
+      desc = "Lazygit (cwd)",
+    },
+    {
+      "<Leader>gf",
+      function()
+        Snacks.lazygit.log_file()
+      end,
+      desc = "Lazygit Current File History",
+    },
+    {
+      "<Leader>gl",
+      function()
+        Snacks.lazygit.log({ cwd = Snacks.git.get_root() })
+      end,
+      desc = "Lazygit Log (Root Dir)",
+    },
+    {
+      "<Leader>gL",
+      function()
+        Snacks.lazygit.log()
+      end,
+      desc = "Lazygit Log (cwd)",
+    },
     -- Terminal
     {
       "<Leader>ft",
@@ -41,6 +78,7 @@ return {
     },
       },
     },
+    lazygit = {},
     statuscolumn = {},
     terminal = {},
   },
