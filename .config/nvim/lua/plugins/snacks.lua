a955d17f3338389b9c6e99bf2a82a2b7e9a4ce11
xian-wen <xianwen_yin@163.com>
2024-12-22 19:24:20 +0800

    Replace copy/paste the whole remote_patterns with insert custom

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -113,20 +113,9 @@ return {
       },
     },
     gitbrowse = {
-      remote_patterns = {
-        { "^(https?://.*)%.git$"              , "%1" },
-        { "^git@(.+):(.+)%.git$"              , "https://%1/%2" },
-        { "^git@(.+):(.+)$"                   , "https://%1/%2" },
-        { "^git@(.+)/(.+)$"                   , "https://%1/%2" },
-        { "^ssh://git@ssh%.(.*)$"             , "https://%1" },
-        { "^ssh://([^:/]+)(:%d+)/(.*)$"       , "https://%1/%3" },
-        { "^ssh://([^/]+)/(.*)$"              , "https://%1/%2" },
-        { "ssh%.dev%.azure%.com/v3/(.*)/(.*)$", "dev.azure.com/%1/_git/%2" },
-        { "^https://%w*@(.*)"                 , "https://%1" },
-        { "^git@(.*)"                         , "https://%1" },
-        { ":%d+"                              , "" },
-        { "%.git$"                            , "" },
-      },
+      config = function(opts, defaults)
+        table.insert(opts.remote_patterns, 1, { "^ssh://git@ssh%.(.*)$", "https://%1" })
+      end,
     },
     lazygit = {},
     statuscolumn = {},
