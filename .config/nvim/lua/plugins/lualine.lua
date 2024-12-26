0944ed27b0444bb64e5f881140a1e7ccc8f8af04
xian-wen <xianwen_yin@163.com>
2024-12-26 15:50:03 +0800

    Add icons for diff and diagnostics in lualine.lua

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,26 +5,57 @@ return {
     -- Hide the statusline on the starter page.
     vim.o.laststatus = 0
   end,
-  opts = {
-    options = {
-      component_separators = { left = "|", right = "|" },
-      section_separators = { left = "", right = "" },
-      disabled_filetypes = {
-        statusline = { "snacks_dashboard" },
+  opts = function()
+    local icons = require("config").icons
+    return {
+      options = {
+        component_separators = { left = "|", right = "|" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {
+          statusline = { "snacks_dashboard" },
+        },
+        globalstatus = true,
       },
-      globalstatus = true,
-    },
-    sections = {
-      lualine_a = {
-        {
-          "mode",
-          -- Add Vim/Neovim logo before mode.
-          fmt = function(str)
-            local logo = " " --  / 
-            return logo .. str
-          end
+      sections = {
+        lualine_a = {
+          {
+            "mode",
+            fmt = function(str)
+              return icons.vim .. str
+            end,
+          },
+        },
+        lualine_b = {
+          "branch",
+          {
+            "diff",
+            symbols = {
+              added = icons.git.added,
+              modified = icons.git.modified,
+              removed = icons.git.removed,
+            },
+            source = function()
+              local gitsigns = vim.b.gitsigns_status_dict
+              if gitsigns then
+                return {
+                  added = gitsigns.added,
+                  modified = gitsigns.changed,
+                  removed = gitsigns.removed,
+                }
+              end
+            end,
+          },
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.ERROR,
+              warn = icons.diagnostics.WARN,
+              info = icons.diagnostics.INFO,
+              hint = icons.diagnostics.HINT,
+            },
+          },
         },
       },
-    },
-  },
+    }
+  end,
 }
