cd026c568e9cb133d5eac805e86dd00bc8fb773e
xian-wen <xianwen_yin@163.com>
2025-01-01 22:27:45 +0800

    Add mini.ai

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,5 +1,117 @@
 return {
   {
+    "echasnovski/mini.ai",
+    event = "VeryLazy",
+    opts = function()
+      local gen_spec = require("mini.ai").gen_spec
+      return {
+        custom_textobjects = {
+          o = gen_spec.treesitter({ -- code block
+            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+          }),
+          f = gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+          c = gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+          d = { "%f[%d]%d+" }, -- digits
+          e = { -- word with camel case
+            {
+              "%u[%l%d]+%f[^%l%d]",
+              "%f[%S][%l%d]+%f[^%l%d]",
+              "%f[%P][%l%d]+%f[^%l%d]",
+              "^[%l%d]+%f[^%l%d]",
+            },
+            "^().*()$",
+          },
+          -- Taken from MiniExtra.gen_ai_spec.buffer
+          g = function(ai_type) -- whole buffer
+            local from_line, to_line = 1, vim.fn.line("$")
+            if ai_type == "i" then
+              -- Skip first and last blank lines for `i` textobject.
+              local first_nonblank = vim.fn.nextnonblank(from_line)
+              local last_nonblank = vim.fn.prevnonblank(to_line)
+              -- Do nothing for buffer with all blanks.
+              if first_nonblank == 0 or last_nonblank == 0 then
+                return { from = { line = from_line, col = 1 } }
+              end
+              from_line, to_line = first_nonblank, last_nonblank
+            end
+            local to_col = math.max(vim.fn.getline(to_line):len(), 1)
+            return {
+              from = { line = from_line, col = 1 },
+              to = { line = to_line, col = to_col },
+            }
+          end,
+          u = gen_spec.function_call(), -- u for "Usage"
+          U = gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
+        },
+        mappings = {
+          around_next = "an",
+          inside_next = "in",
+          around_last = "ap",
+          inside_last = "ip",
+        },
+        -- Number of lines within which textobject is searched.
+        n_lines = 500,
+      }
+    end,
+    config = function(_, opts)
+      require("mini.ai").setup(opts)
+      local objects = {
+        { " ", desc = "whitespace" },
+        { "'", desc = "' string" },
+        { "(", desc = "() block" },
+        { ")", desc = "() block with ws" },
+        { "<", desc = "<> block" },
+        { ">", desc = "<> block with ws" },
+        { "?", desc = "user prompt" },
+        { "U", desc = "use/call without dot" },
+        { "[", desc = "[] block" },
+        { "]", desc = "[] block with ws" },
+        { "_", desc = "underscore" },
+        { "`", desc = "` string" },
+        { "a", desc = "argument" },
+        { "b", desc = ")]} block" },
+        { "c", desc = "class" },
+        { "d", desc = "digit(s)" },
+        { "e", desc = "CamelCase / snake_case" },
+        { "f", desc = "function" },
+        { "g", desc = "entire file" },
+        { "i", desc = "indent" },
+        { "o", desc = "block, conditional, loop" },
+        { "q", desc = "quote `\"'" },
+        { "t", desc = "tag" },
+        { "u", desc = "use/call" },
+        { "{", desc = "{} block" },
+        { "}", desc = "{} with ws" },
+        { '"', desc = '" string' },
+      }
+      local ret = { mode = { "o", "x" } }
+      local mappings = vim.tbl_extend("force", {}, {
+        around = "a",
+        inside = "i",
+        around_next = "an",
+        inside_next = "in",
+        around_last = "ap",
+        inside_last = "ip",
+      }, opts.mappings or {})
+      mappings.goto_left = nil
+      mappings.goto_right = nil
+      for name, prefix in pairs(mappings) do
+        name = name:gsub("^around_", ""):gsub("^inside_", "")
+        ret[#ret + 1] = { prefix, group = name }
+        for _, obj in ipairs(objects) do
+          local desc = obj.desc
+          if prefix:sub(1, 1) == "i" then
+            desc = desc:gsub(" with ws", "")
+          end
+          ret[#ret + 1] = { prefix .. obj[1], desc = obj.desc }
+        end
+      end
+      require("which-key").add(ret, { notify = false })
+    end,
+  },
+
+  {
     "echasnovski/mini.icons",
     opts = {},
     init = function()
