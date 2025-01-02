02262815029ac9e37380a4451494bbcf719a076b
xian-wen <xianwen_yin@163.com>
2025-01-02 23:12:23 +0800

    Add .stylua.toml to make conform format as expected

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,53 +1,49 @@
 return {
-	"stevearc/conform.nvim",
-	dependencies = { "mason.nvim" },
-	-- Format on save.
-	event = "BufWritePre",
-	cmd = "ConformInfo",
-	keys = {
-		{
-			"<Leader>cf",
-			function()
-				require("conform").format()
-			end,
-			mode = { "n", "v" },
-			desc = "Format",
-		},
-		{
-			"<Leader>cF",
-			function()
-				require("conform").format({ formatters = { "injected" } })
-			end,
-			mode = { "n", "v" },
-			desc = "Format Injected Langs",
-		},
-	},
-	opts = {
-		default_format_opts = {
-			timeout_ms = 3000,
-			lsp_format = "fallback",
-		},
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_format = "fallback",
-		},
-		formatters_by_ft = {
-			lua = { "stylua" },
-			sh = { "shfmt" },
-		},
-		-- Custom formatters and overrides for built-in formatters.
-		formatters = {
-			-- Format treesitter injected languages, such as markdown code blocks.
-			injected = {
-				options = { ignore_errors = true },
-			},
-			shfmt = {
-				-- Indent with 2 spaces and indent switch cases.
-				prepend_args = { "-i", "2", "-ci" },
-			},
-		},
-	},
-	init = function()
-		vim.o.formatexpr = "v:lua.require('conform').formatexpr()"
-	end,
+  "stevearc/conform.nvim",
+  dependencies = { "mason.nvim" },
+  -- Format on save.
+  event = "BufWritePre",
+  cmd = "ConformInfo",
+  keys = {
+    {
+      "<Leader>cf",
+      function()
+        require("conform").format()
+      end,
+      mode = { "n", "v" },
+      desc = "Format",
+    },
+    {
+      "<Leader>cF",
+      function()
+        require("conform").format({ formatters = { "injected" } })
+      end,
+      mode = { "n", "v" },
+      desc = "Format Injected Langs",
+    },
+  },
+  opts = {
+    -- Change the default values when calling conform.format(),
+    -- also affect the default values for format_on_save/format_after_save.
+    default_format_opts = { lsp_format = "fallback" },
+    format_on_save = { timeout_ms = 500 },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      sh = { "shfmt" },
+    },
+    -- Custom formatters and overrides for built-in formatters.
+    formatters = {
+      -- Format treesitter injected languages, such as markdown code blocks.
+      injected = {
+        options = { ignore_errors = true },
+      },
+      shfmt = {
+        -- Indent with 2 spaces and indent switch cases.
+        prepend_args = { "-i", "2", "-ci" },
+      },
+    },
+  },
+  init = function()
+    vim.o.formatexpr = "v:lua.require('conform').formatexpr()"
+  end,
 }
