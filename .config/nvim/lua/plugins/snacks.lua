a663aac012c0bcf140a76e8834bd31c8f61be039
xian-wen <xianwen_yin@163.com>
2025-01-25 08:38:25 +0000

    Add keymap for neovim news

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -155,6 +155,25 @@ return {
       end,
       desc = "Toggle Zoom",
     },
+    -- Neovim News
+    {
+      "<Leader>N",
+      desc = "Neovim News",
+      function()
+        Snacks.win({
+          file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+          width = 0.6,
+          height = 0.6,
+          wo = {
+            spell = false,
+            wrap = false,
+            signcolumn = "yes",
+            statuscolumn = " ",
+            conceallevel = 3,
+          },
+        })
+      end,
+    },
   },
   opts = {
     bigfile = {},
