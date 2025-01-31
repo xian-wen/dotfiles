692b772a49e2cfe13212d20f680488cc4bfd414a
xian-wen <xianwen_yin@163.com>
2025-01-31 08:25:38 +0000

    Move neovim news into Snacks.win

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -12,26 +12,6 @@ return {
     "folke/snacks.nvim",
     lazy = false,
     priority = 1000,
-    keys = {
-      {
-        "<Leader>N",
-        function()
-          Snacks.win({
-            file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-            width = 0.6,
-            height = 0.6,
-            wo = {
-              spell = false,
-              wrap = false,
-              signcolumn = "yes",
-              statuscolumn = " ",
-              conceallevel = 3,
-            },
-          })
-        end,
-        desc = "Neovim News",
-      },
-    },
     opts = {
       bigfile = {},
       quickfile = {},
@@ -561,6 +541,34 @@ return {
           },
         },
       },
+    },
+  },
+
+  -- Win
+  {
+    "folke/snacks.nvim",
+    keys = {
+      {
+        "<Leader>N",
+        function()
+          Snacks.win({
+            file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+            width = 0.6,
+            height = 0.6,
+            wo = {
+              spell = false,
+              wrap = false,
+              signcolumn = "yes",
+              statuscolumn = " ",
+              conceallevel = 3,
+            },
+          })
+        end,
+        desc = "Neovim News",
+      },
+    },
+    opts = {
+      win = {},
     },
   },
 
