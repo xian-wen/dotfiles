39ca06f05ab68e1f86048a5cdf214c943ef9ce5d
xian-wen <xianwen_yin@163.com>
2025-01-31 08:34:32 +0000

    Add flash support for Snacks.picker

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -513,6 +513,8 @@ return {
           input = {
             keys = {
               ["<A-c>"] = { "toggle_cwd", mode = { "n", "i" } },
+              ["<A-s>"] = { "flash", mode = { "n", "i" } },
+              ["s"] = { "flash" },
             },
           },
         },
@@ -523,6 +525,24 @@ return {
             local current = picker:cwd()
             picker:set_cwd(current == root and cwd or root)
             picker:find()
+          end,
+          flash = function(picker)
+            require("flash").jump({
+              pattern = "^",
+              label = { after = { 0, 0 } },
+              search = {
+                mode = "search",
+                exclude = {
+                  function(win)
+                    return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "snacks_picker_list"
+                  end,
+                },
+              },
+              action = function(match)
+                local idx = picker.list:row2idx(match.pos[1])
+                picker.list:_move(idx, true, true)
+              end,
+            })
           end,
         },
       },
