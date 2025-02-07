6b3e761739f131580e3900df8c6329261a8dfb8d
xian-wen <xianwen_yin@163.com>
2025-02-07 08:41:31 +0000

    Remove lsp_root since lsp client might be reused by neovim

--- .config/nvim/lua/util/init.lua
+++ .config/nvim/lua/util/init.lua
@@ -21,12 +21,10 @@ M.opts = function(name)
   return plugin and require("lazy.core.plugin").values(plugin, "opts", false) or {}
 end
 
----@param opts? {buf?: number}
+---@param opts? { buf?: number }
 M.root = function(opts)
   opts = opts or {}
-  local clients = vim.lsp.get_clients({ bufnr = opts.buf or 0 })
-  local lsp_root = clients[1] and clients[1].config.root_dir
-  return lsp_root or vim.fs.root(0, { ".git", "lua" }) or vim.uv.cwd()
+  return vim.fs.root(opts.buf or 0, { ".git", "lua" }) or vim.uv.cwd()
 end
 
 M.git_root = function()
