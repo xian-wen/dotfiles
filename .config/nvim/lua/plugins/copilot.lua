00a8f88662a5847957cffa0b8ff3087543bda7d5
xian-wen <xianwen_yin@163.com>
2025-02-08 10:24:55 +0000

    Replace InsertEnter with BufReadPost for copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -2,7 +2,7 @@ return {
   {
     "zbirenbaum/copilot.lua",
     build = ":Copilot auth",
-    event = "InsertEnter",
+    event = "BufReadPost",
     cmd = "Copilot",
     opts = {
       panel = { enabled = false },
