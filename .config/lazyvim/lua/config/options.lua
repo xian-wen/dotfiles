ade34b966a9225bc7d60105239bff5ca62b24a1a
xian-wen <xianwen_yin@163.com>
2025-02-14 11:51:02 +0000

    Disable Snacks animation in LazyVim

--- .config/lazyvim/lua/config/options.lua
+++ .config/lazyvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local g = vim.g
 local opt = vim.opt
+
+g.snacks_animate = false
 
 opt.clipboard = "unnamedplus" -- Sync with system clipboard
