b8c6d7809bab510cc5ea04f40ba38afcc3be7945
xian-wen <xianwen_yin@163.com>
2025-07-09 17:37:58 +0800

    Remove keymap <Leader>cd, which is <C-w>d by default

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -80,7 +80,7 @@ map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
 map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
 map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
 map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
-map("n", "<Leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+-- <C-w>d: Line Diagnostics
 
 -- Highlights under cursor
 map("n", "<Leader>ui", vim.show_pos, { desc = "Inspect Pos" })
