af7e1163c5ae12d3e9bb7f5b654e7eaaacfb98d7
xian-wen <xianwen_yin@163.com>
2025-07-09 17:43:29 +0800

    Replace treesitter go to parameter from a to p and A to P

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -60,22 +60,22 @@ return {
           goto_next_start = {
             ["]f"] = "@function.outer",
             ["]c"] = "@class.outer",
-            ["]a"] = "@parameter.inner",
+            ["]p"] = "@parameter.inner",
           },
           goto_next_end = {
             ["]F"] = "@function.outer",
             ["]C"] = "@class.outer",
-            ["]A"] = "@parameter.inner",
+            ["]P"] = "@parameter.inner",
           },
           goto_previous_start = {
             ["[f"] = "@function.outer",
             ["[c"] = "@class.outer",
-            ["[a"] = "@parameter.inner",
+            ["[p"] = "@parameter.inner",
           },
           goto_previous_end = {
             ["[F"] = "@function.outer",
             ["[C"] = "@class.outer",
-            ["[A"] = "@parameter.inner",
+            ["[P"] = "@parameter.inner",
           },
         },
       },
