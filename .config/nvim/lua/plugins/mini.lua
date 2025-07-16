62e2072beac7640376bd796285993a3b97f2c7d2
xian-wen <xianwen_yin@163.com>
2025-07-16 20:19:09 +0800

    Lazily add mini.ai keymaps into which-key

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -50,58 +50,60 @@ return {
     end,
     config = function(_, opts)
       require("mini.ai").setup(opts)
-      local objects = {
-        { " ", desc = "whitespace" },
-        { "'", desc = "' string" },
-        { "(", desc = "() block" },
-        { ")", desc = "() block with ws" },
-        { "<", desc = "<> block" },
-        { ">", desc = "<> block with ws" },
-        { "?", desc = "user prompt" },
-        { "U", desc = "use/call without dot" },
-        { "[", desc = "[] block" },
-        { "]", desc = "[] block with ws" },
-        { "_", desc = "underscore" },
-        { "`", desc = "` string" },
-        { "a", desc = "argument" },
-        { "b", desc = ")]} block" },
-        { "c", desc = "class" },
-        { "d", desc = "digit(s)" },
-        { "e", desc = "CamelCase / snake_case" },
-        { "f", desc = "function" },
-        { "g", desc = "entire file" },
-        { "i", desc = "indent" },
-        { "o", desc = "block, conditional, loop" },
-        { "q", desc = "quote `\"'" },
-        { "t", desc = "tag" },
-        { "u", desc = "use/call" },
-        { "{", desc = "{} block" },
-        { "}", desc = "{} with ws" },
-        { '"', desc = '" string' },
-      }
-      local ret = { mode = { "o", "x" } }
-      local mappings = vim.tbl_extend("force", {}, {
-        around = "a",
-        inside = "i",
-        around_next = "an",
-        inside_next = "in",
-        around_last = "al",
-        inside_last = "il",
-      }, opts.mappings or {})
-      mappings.goto_left = nil
-      mappings.goto_right = nil
-      for name, prefix in pairs(mappings) do
-        name = name:gsub("^around_", ""):gsub("^inside_", "")
-        ret[#ret + 1] = { prefix, group = name }
-        for _, obj in ipairs(objects) do
-          local desc = obj.desc
-          if prefix:sub(1, 1) == "i" then
-            desc = desc:gsub(" with ws", "")
+      vim.schedule(function()
+        local objects = {
+          { " ", desc = "whitespace" },
+          { "'", desc = "' string" },
+          { "(", desc = "() block" },
+          { ")", desc = "() block with ws" },
+          { "<", desc = "<> block" },
+          { ">", desc = "<> block with ws" },
+          { "?", desc = "user prompt" },
+          { "U", desc = "use/call without dot" },
+          { "[", desc = "[] block" },
+          { "]", desc = "[] block with ws" },
+          { "_", desc = "underscore" },
+          { "`", desc = "` string" },
+          { "a", desc = "argument" },
+          { "b", desc = ")]} block" },
+          { "c", desc = "class" },
+          { "d", desc = "digit(s)" },
+          { "e", desc = "CamelCase / snake_case" },
+          { "f", desc = "function" },
+          { "g", desc = "entire file" },
+          { "i", desc = "indent" },
+          { "o", desc = "block, conditional, loop" },
+          { "q", desc = "quote `\"'" },
+          { "t", desc = "tag" },
+          { "u", desc = "use/call" },
+          { "{", desc = "{} block" },
+          { "}", desc = "{} with ws" },
+          { '"', desc = '" string' },
+        }
+        local ret = { mode = { "o", "x" } }
+        local mappings = vim.tbl_extend("force", {}, {
+          around = "a",
+          inside = "i",
+          around_next = "an",
+          inside_next = "in",
+          around_last = "al",
+          inside_last = "il",
+        }, opts.mappings or {})
+        mappings.goto_left = nil
+        mappings.goto_right = nil
+        for name, prefix in pairs(mappings) do
+          name = name:gsub("^around_", ""):gsub("^inside_", "")
+          ret[#ret + 1] = { prefix, group = name }
+          for _, obj in ipairs(objects) do
+            local desc = obj.desc
+            if prefix:sub(1, 1) == "i" then
+              desc = desc:gsub(" with ws", "")
+            end
+            ret[#ret + 1] = { prefix .. obj[1], desc = obj.desc }
           end
-          ret[#ret + 1] = { prefix .. obj[1], desc = obj.desc }
         end
-      end
-      require("which-key").add(ret, { notify = false })
+        require("which-key").add(ret, { notify = false })
+      end)
     end,
   },
 
