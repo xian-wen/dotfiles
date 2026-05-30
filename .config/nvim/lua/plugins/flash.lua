10e84aed495625998e1182cc99b0338d1f04e4c6
xian-wen <xianwen_yin@163.com>
2026-05-30 18:59:14 +0800

    Move flash config in snacks.lua into flash.lua

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -1,54 +1,92 @@
 return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  keys = {
-    {
-      "s",
-      function()
-        require("flash").jump()
-      end,
-      mode = { "n", "x", "o" },
-      desc = "Flash",
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "s",
+        function()
+          require("flash").jump()
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Flash",
+      },
+      {
+        "S",
+        function()
+          require("flash").treesitter()
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        function()
+          require("flash").remote()
+        end,
+        mode = "o",
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        function()
+          require("flash").treesitter_search()
+        end,
+        mode = { "o", "x" },
+        desc = "Treesitter Search",
+      },
+      {
+        "<C-s>",
+        function()
+          require("flash").toggle()
+        end,
+        mode = "c",
+        desc = "Toggle Flash Search",
+      },
     },
-    {
-      "S",
-      function()
-        require("flash").treesitter()
-      end,
-      mode = { "n", "x", "o" },
-      desc = "Flash Treesitter",
-    },
-    {
-      "r",
-      function()
-        require("flash").remote()
-      end,
-      mode = "o",
-      desc = "Remote Flash",
-    },
-    {
-      "R",
-      function()
-        require("flash").treesitter_search()
-      end,
-      mode = { "o", "x" },
-      desc = "Treesitter Search",
-    },
-    {
-      "<C-s>",
-      function()
-        require("flash").toggle()
-      end,
-      mode = "c",
-      desc = "Toggle Flash Search",
+    opts = {
+      search = {
+        exclude = { "blink-cmp-menu" },
+      },
+      label = {
+        rainbow = { enabled = true },
+      },
     },
   },
-  opts = {
-    search = {
-      exclude = { "blink-cmp-menu" },
-    },
-    label = {
-      rainbow = { enabled = true },
+
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        win = {
+          input = {
+            keys = {
+              ["<A-s>"] = { "flash", mode = { "n", "i" } },
+              ["s"] = { "flash" },
+            },
+          },
+        },
+        actions = {
+          flash = function(picker)
+            require("flash").jump({
+              pattern = "^",
+              label = { after = { 0, 0 } },
+              search = {
+                mode = "search",
+                exclude = {
+                  function(win)
+                    return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "snacks_picker_list"
+                  end,
+                },
+              },
+              action = function(match)
+                local idx = picker.list:row2idx(match.pos[1])
+                picker.list:_move(idx, true, true)
+              end,
+            })
+          end,
+        },
+      },
     },
   },
 }
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -589,8 +589,6 @@ return {
           input = {
             keys = {
               ["<A-c>"] = { "toggle_cwd", mode = { "n", "i" } },
-              ["<A-s>"] = { "flash", mode = { "n", "i" } },
-              ["s"] = { "flash" },
               ["<A-t>"] = { "trouble_open", mode = { "n", "i" } },
             },
           },
@@ -602,24 +600,6 @@ return {
             local current = picker:cwd()
             picker:set_cwd(current == root and cwd or root)
             picker:find()
-          end,
-          flash = function(picker)
-            require("flash").jump({
-              pattern = "^",
-              label = { after = { 0, 0 } },
-              search = {
-                mode = "search",
-                exclude = {
-                  function(win)
-                    return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "snacks_picker_list"
-                  end,
-                },
-              },
-              action = function(match)
-                local idx = picker.list:row2idx(match.pos[1])
-                picker.list:_move(idx, true, true)
-              end,
-            })
           end,
           trouble_open = function(...)
             return require("trouble.sources.snacks").actions.trouble_open.action(...)
